c19d977e7a483e805e1b2aedfaa4ecf0d20162fc
Dell <farros@rambler.ru>
2018-10-17 10:15:09 +0300

    commit

--- src/app/modules/guides/components/push-notification/common-patterns/common-patterns.component.ts
+++ src/app/modules/guides/components/push-notification/common-patterns/common-patterns.component.ts
@@ -11,12 +11,14 @@ export class CommonPatternsComponent implements AfterViewInit {
 
 
     private localMenu : SideNavItem[] = [
-        new SideNavItem({text:"Поведение оповещения",href: '/guides/push-notification/notification-behavior', fragment:'notification-behavior'}),
-        new SideNavItem({text:"Actions (действия)",href: '/guides/push-notification/notification-behavior', fragment:'Actions'}),
-        new SideNavItem({text:"Tag (тег)",href: '/guides/push-notification/notification-behavior', fragment:'Tag'}),
-        new SideNavItem({text:"Renotify (сигнализировать)",href: '/guides/push-notification/notification-behavior', fragment:'Renotify'}),
-        new SideNavItem({text:"Silent (не сигнализировать)",href: '/guides/push-notification/notification-behavior', fragment:'Silent'}),
-        new SideNavItem({text:"Необходимость взаимодействия",href: '/guides/push-notification/notification-behavior', fragment:'requireInteraction'}),
+        new SideNavItem({text:"Общие шаблоны оповещений",href: '/guides/push-notification/common-patterns', fragment:'commonTemplates'}),
+        new SideNavItem({text:"Событие закрытия уведомления",href: '/guides/push-notification/common-patterns', fragment: 'notificationclose'}),
+        new SideNavItem({text:"Добавление данных",href: '/guides/push-notification/common-patterns', fragment:'AddingData'}),
+        new SideNavItem({text:"Открытие окна",href: '/guides/push-notification/common-patterns', fragment:'openWindow'}),
+        new SideNavItem({text:"Фокус на окно",href: '/guides/push-notification/common-patterns', fragment:'focusExWindow'}),
+        new SideNavItem({text:"Объединение оповещений",href: '/guides/push-notification/common-patterns', fragment:'mergingNotifications'}),
+        new SideNavItem({text:"Исключение из правил",href: '/guides/push-notification/common-patterns', fragment:'exceptionRule'}),
+        new SideNavItem({text:"Сообщение окну",href: '/guides/push-notification/common-patterns', fragment:'messagePage'}),
 
     ];
     @ViewChildren(QuerySelectorDirective, {read: ElementRef}) private appAnchors: QueryList<ElementRef>;//обеспечение навигации по странице-обязателен для всех страниц, имеющих локальное меню
@@ -24,7 +26,7 @@ export class CommonPatternsComponent implements AfterViewInit {
     constructor(private communication : CommunicationService){}
 
     ngAfterViewInit(){
-        this.communication.sendResource({type : 'resource', appHeader : 'Общие шаблоны уведомлений', localData : { appAnchors : this.appAnchors, localMenu : this.localMenu}})
+        this.communication.sendResource({type : 'resource', appHeader : 'Общие програмные шаблоны оповещений', localData : { appAnchors : this.appAnchors, localMenu : this.localMenu}})
     }
 
 }
